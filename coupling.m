4583bdf7eda4c1cd21eec2d49f871efd68059a5b
macshine <macshine@stanford.edu>
2015-06-14 22:55:37 -1000

    Update coupling.m

--- coupling.m
+++ coupling.m
@@ -3,46 +3,60 @@
 %data: should be organized in 'time x nodes' matrix
 %smooth: smoothing parameter for simple moving average of coupling metric
 
-function [fc, sma] = coupling(data,smooth)
 
-%define variables to be used later
+function [fc, sma] = coupling(data,window)
 
+%define variables
 [ts,nodes] = size(data);
 der = ts - 1;
 
 
-%calculate the temporal derivative of each timeseries
-
+%calculate temporal derivative
 td = diff(data);
 
 
-%standardize the data
+%standardize data
+data_std = std(td);
 
-data_std = std(data);
 for i = 1:nodes
     td(:,i) = td(:,i) / data_std(1,i);
 end
 
 
-%calculate the functional coupling score
+% [...] = zscore(X,FLAG,DIM) standardizes X by working along the dimension
+%    DIM of X. Pass in FLAG==0 to use the default normalization by N-1, or 1
+%    to use N.
 
-fc = zeros(der,nodes,nodes);
-for t = 1:der
-    for i = 1:nodes
-        for j = 1:nodes
-            fc(t,i,j) = td(t,i) .* td(t,j);
-        end
+
+%functional coupling score
+
+fc = bsxfun(@times,permute(td,[1,3,2]),permute(td,[1,2,3]));
+
+
+
+%simple moving average
+
+sma_filter = 1/window*ones(window,1);
+
+sma = zeros(ts,nodes,nodes);
+
+for j = 1:nodes
+    
+    for k = 1:nodes
+        
+        sma(2:end,j,k) = filter(sma_filter,1,fc(:,j,k));
+        
     end
+    
 end
 
 
-%temporal smoothing
-%the 'tsmovavg' function only works on 2d data, so I've reshaped the matrix briefly to allow this
+sma = permute(sma,[2,3,1]);
 
-temp = reshape(fc,der,nodes^2);
-sma = tsmovavg(temp, 's', smooth, 1);
-sma = reshape(sma,der,nodes,nodes);
+sma(:,:,1) = sma(:,:,2);
 
 end
+
+
 
 
