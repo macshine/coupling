ed2f5f226a8a08d78b126d03ecb296dc8b0225c6
Mac Shine <macshine@stanford.edu>
2016-05-04 20:31:52 -0700

    Update coupling.m

--- coupling.m
+++ coupling.m
@@ -1,10 +1,16 @@
+function mtd = coupling(data,window)
+%COUPLING       Multiplication of Temporal Derivatives
+%
+%  mtd = coupling(data,window);
+%
+%  This code creates a time-resolved connectivity matrix for a given window
+%  length. See http://www.ncbi.nlm.nih.gov/pubmed/26231247 for more details.
+%
+%  Input:      data     time-series organized in 'time x nodes' matrix
+%              window   window length (in time-points)
+%
+%  Output:     mtd      time-resolved connectivity matrix
 
-%creates a functional coupling metric from an input matrix 'data'
-%data: should be organized in 'time x nodes' matrix
-%smooth: smoothing parameter for simple moving average of coupling metric
-
-
-function [mtd, sma] = coupling(data,window)
 
 %define variables
 [ts,nodes] = size(data);
@@ -18,37 +24,46 @@ td = diff(data);
 %standardize data
 data_std = std(td);
 
+
 for i = 1:nodes
+    
     td(:,i) = td(:,i) / data_std(1,i);
+    
 end
+
+
+% [...] = zscore(X,FLAG,DIM) standardizes X by working along the dimension
+%    DIM of X. Pass in FLAG==0 to use the default normalization by N-1, or 1
+%    to use N.
+
 
 %functional coupling score
 
-mtd = bsxfun(@times,permute(td,[1,3,2]),permute(td,[1,2,3]));
+fc = bsxfun(@times,permute(td,[1,3,2]),permute(td,[1,2,3]));
+
+
 
 %simple moving average
 
-sma_filter = 1/window*ones(window,1);
+mtd_filter = 1/window*ones(window,1);
 
-sma = zeros(ts,nodes,nodes);
+mtd = zeros(ts,nodes,nodes);
 
 for j = 1:nodes
     
     for k = 1:nodes
         
-        sma(2:end,j,k) = filter(sma_filter,1,mtd(:,j,k));
+        mtd(2:end,j,k) = filter(mtd_filter,1,fc(:,j,k));
         
     end
     
 end
 
 
-sma = permute(sma,[2,3,1]);
+mtd = permute(mtd,[2,3,1]);
 
-sma(:,:,1) = sma(:,:,2);
+mtd(:,:,1) = mtd(:,:,2);
 
 end
-
-
 
 
